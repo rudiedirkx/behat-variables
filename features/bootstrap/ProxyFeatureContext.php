2c98030617f2c7d6f2834150018788e8dc9f1db7
Rudie Dirkx <github@hotblocks.nl>
2016-03-28 03:03:29 +0200

    +proxy (process) test

---
+++ features/bootstrap/ProxyFeatureContext.php
@@ -0,0 +1,191 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
+
+class ProxyFeatureContext implements Context, SnippetAcceptingContext {
+
+	private $phpBin = '';
+	private $process;
+	private $workingDir = '';
+
+
+
+	/**
+	 * Initializes context.
+	 */
+	public function __construct() {
+
+	}
+
+
+
+	/**
+	 * Cleans test folders in the temporary directory.
+	 *
+	 * @BeforeSuite
+	 * @AfterSuite
+	 */
+	public static function cleanTestFolders() {
+		if (is_dir($dir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'behat')) {
+			self::clearDirectory($dir);
+		}
+	}
+
+	/**
+	 * Prepares test folders in the temporary directory.
+	 *
+	 * @BeforeScenario
+	 */
+	public function prepareTestFolders() {
+		$dir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'behat' . DIRECTORY_SEPARATOR . md5(microtime() * rand(0, 10000));
+
+		mkdir($dir . '/features/bootstrap/i18n', 0777, true);
+
+		$phpFinder = new PhpExecutableFinder();
+		if (false === $php = $phpFinder->find()) {
+			throw new \RuntimeException('Unable to find the PHP executable.');
+		}
+		$this->workingDir = $dir;
+		$this->phpBin = $php;
+		$this->process = new Process(null);
+	}
+
+
+
+	/**
+	 * @Given /^(?:there is )?a file named "([^"]*)" with:$/
+	 */
+	public function aFileNamedWith($filename, PyStringNode $content) {
+		$content = strtr((string) $content, array("'''" => '"""'));
+		$this->createFile($this->workingDir . '/' . $filename, $content);
+	}
+
+	/**
+	 * Runs behat command with provided parameters
+	 *
+	 * @When /^I run "behat(?: ((?:\"|[^"])*))?"$/
+	 * @When /^I run behat$/
+	 *
+	 * @param   string $argumentsString
+	 */
+	public function iRunBehat($argumentsString = '') {
+		$argumentsString = strtr($argumentsString, array('\'' => '"'));
+
+		$this->process->setWorkingDirectory($this->workingDir);
+		$command = sprintf(
+			'%s %s %s %s',
+			$this->phpBin,
+			escapeshellarg(BEHAT_BIN_PATH),
+			$argumentsString,
+			strtr('--no-colors --strict --format=progress --format-settings=\'{"timer": false}\'', array(
+				'\'' => '"',
+				'"' => '\"',
+			))
+		);
+		$this->process->setCommandLine($command);
+
+		// Don't reset the LANG variable on HHVM, because it breaks HHVM itself
+		if (!defined('HHVM_VERSION')) {
+			$env = $this->process->getEnv();
+			$env['LANG'] = 'en'; // Ensures that the default language is en, whatever the OS locale is.
+			$this->process->setEnv($env);
+		}
+
+		$this->process->start();
+		$this->process->wait();
+	}
+
+	/**
+	 * @Then /^it should (pass|fail) with:$/
+	 */
+	public function itShouldPassWith($shouldPassFail, PyStringNode $string) {
+		$shouldPass = $shouldPassFail == 'pass';
+		$passed = $this->getExitCode() == 0;
+
+		$output = trim($this->getOutput());
+
+		$did = $passed ? 'passed' : 'failed';
+		$should = $shouldPass ? 'pass' : 'fail';
+
+		if ($this->getExitCode() == (int) $shouldPass) {
+			echo $output;
+			throw new \Exception("Scenario $did, but should $should");
+		}
+
+		if ($this->cleanOutput($output) != $this->cleanOutput($string)) {
+			echo $output;
+			throw new \Exception("Scenario $did, but with wrong output");
+		}
+	}
+
+
+
+	/**
+	 *
+	 */
+	protected function cleanOutput($output) {
+		return preg_replace('#([\r\n]+)[ \t]+#', '$1', trim($output));
+	}
+
+	/**
+	 *
+	 */
+	private function getOutput() {
+		$output = $this->process->getErrorOutput() . $this->process->getOutput();
+
+		// Normalize the line endings in the output
+		if ("\n" !== PHP_EOL) {
+			$output = str_replace(PHP_EOL, "\n", $output);
+		}
+
+		// Replace wrong warning message of HHVM
+		$output = str_replace('Notice: Undefined index: ', 'Notice: Undefined offset: ', $output);
+
+		return trim(preg_replace("/ +$/m", '', $output));
+	}
+
+	/**
+	 *
+	 */
+	private function getExitCode() {
+		return $this->process->getExitCode();
+	}
+
+	/**
+	 *
+	 */
+	private function createFile($filename, $content) {
+		$path = dirname($filename);
+		if (!is_dir($path)) {
+			mkdir($path, 0777, true);
+		}
+
+		file_put_contents($filename, $content);
+	}
+
+	/**
+	 *
+	 */
+	private static function clearDirectory($path) {
+		$files = scandir($path);
+		array_shift($files);
+		array_shift($files);
+
+		foreach ($files as $file) {
+			$file = $path . DIRECTORY_SEPARATOR . $file;
+			if (is_dir($file)) {
+				self::clearDirectory($file);
+			} else {
+				unlink($file);
+			}
+		}
+
+		rmdir($path);
+	}
+
+}
---
+++ features/proxy.feature
@@ -0,0 +1,108 @@
+Feature: Simple case
+
+	Background:
+		Given a file named "behat.yml" with:
+			"""
+			default:
+			  suites:
+			    default:
+			      contexts:
+			        - rdx\behatvars\BehatVariablesContext
+			        - FeatureContext
+			  extensions:
+			      rdx\behatvars\BehatVariablesExtension: ~
+			"""
+		And a file named "features/bootstrap/FeatureContext.php" with:
+			"""
+			<?php
+
+			use Behat\Behat\Context\Context;
+			use Behat\Behat\Context\SnippetAcceptingContext;
+
+			class FeatureContext implements Context, SnippetAcceptingContext {
+			  /** @Given a value :value */
+			  public function aValue($value) {
+			    return $value;
+			  }
+
+			  /** @Then :value1 should equal :value2 */
+			  public function shouldEqual($value1, $value2) {
+			    if ($value1 != $value2) {
+			      throw new \Exception("value1 ($value1) does not equal value2 ($value2)");
+			    }
+			  }
+			}
+			"""
+		And   a file named "features/test.feature" with:
+			"""
+			Feature: Remember a variable
+
+			  Scenario: Remember a variable
+			    Given a value "50"
+			    And I save it into "X"
+			    Then "<<X>>" should equal "50"
+			"""
+
+	Scenario: Forgetting BehatVariablesExtension
+		Given a file named "behat.yml" with:
+			"""
+			default:
+			  suites:
+			    default:
+			      contexts:
+			        - rdx\behatvars\BehatVariablesContext
+			        - FeatureContext
+			"""
+		When I run behat
+		Then it should fail with:
+			"""
+			..F
+
+			--- Failed steps:
+
+			  Then "<<X>>" should equal "50" # features/test.feature:6
+			    value1 (<<X>>) does not equal value2 (50) (Exception)
+
+			1 scenario (1 failed)
+			3 steps (2 passed, 1 failed)
+			"""
+
+	Scenario: Forgetting BehatVariablesContext
+		Given a file named "behat.yml" with:
+			"""
+			default:
+			  suites:
+			    default:
+			      contexts:
+			        - FeatureContext
+			  extensions:
+			      rdx\behatvars\BehatVariablesExtension: ~
+			"""
+		When I run behat
+		Then it should fail with:
+			"""
+			.U-
+
+			1 scenario (1 undefined)
+			3 steps (1 passed, 1 undefined, 1 skipped)
+
+			--- FeatureContext has missing steps. Define them with these snippets:
+
+			  /**
+			   * @Given I save it into :arg1
+			   */
+			  public function iSaveItInto($arg1)
+			  {
+			    throw new PendingException();
+			  }
+			"""
+
+	Scenario: Remember a variable
+		When I run behat
+		Then it should pass with:
+			"""
+			...
+
+			1 scenario (1 passed)
+			3 steps (3 passed)
+			"""
